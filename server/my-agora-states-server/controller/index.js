11b269e2b1603cfd89cb40d177b6a94be8d8b4c9
sowon_m1 <bsw2377@gmail.com>
2023-06-11 14:09:10 +0900

    update server code

--- server/my-agora-states-server/controller/index.js
+++ server/my-agora-states-server/controller/index.js
@@ -2,10 +2,12 @@ const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
 const handleRequestBody = (req, res) => {
-  if (!req.body) return res.status(400).send("no request body");
-
   const { title, author, bodyHTML } = req.body;
 
+  if (!req.body) {
+    return res.status(400).send("no request body");
+  }
+  
   if (!title && !author && !bodyHTML) {
     return res.status(400).send("bad request");
   }
@@ -37,17 +39,72 @@ const discussionsController = {
   create: (req, res) => {
     const { title, author, bodyHTML } = req.body;
 
+    if (handleRequestBody(req, res) !== true) {
+      return
+    }
 
+    /* request example
+    {
+      "title": "제목입니다.",
+      "author": "죠르디",
+      "bodyHTML": "질문이 있습니다!"
+    } 
+     */
+    const id = parseInt(Math.random() * 1000);
+    const newDiscussion = {
+      id,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      title,
+      url: null,
+      author,
+      answer: null,
+      bodyHTML,
+      avatarUrl: null
+    };
+
+    discussionsData.unshift(newDiscussion);
+    return res.status(201).send(`created id: ${id}`)
   },
 
   updateById: (req, res) => {
+    const { id } = req.params;
+    const bodyData = req.body
 
+    if (handleRequestBody(req, res) !== true) {
+      return
+    }
+
+    // 요청된 id의 index를 찾아 해당하는 데이터를 수정
+    const updatedIdx = discussionsData.findIndex((el) => el.id === Number(id) );
+
+    if (updatedIdx === -1) { // 일치하는 id가 없는 경우
+      return res.status(404).send("Not Found");
+    } else { // 일치하는 id가 있는 경우
+      const updatedData = { 
+        ...discussionsData[updatedIdx], 
+        ...bodyData,
+        updatedAt: new Date().toISOString(), 
+      }
+  
+      discussionsData.splice(updatedIdx, 1, updatedData)
+      return res.status(200).send(`updated id: ${id}`)
+    }
   },
 
   deleteById: (req, res) => {
-    // const { id } = req.params;
+    const { id } = req.params;
 
-    console.log("test")
+    // 요청된 id의 index를 찾아 해당하는 데이터를 삭제
+    const idx = discussionsData.findIndex((el) => el.id === Number(id))
+    console.log("idx", idx) // id:45를 선택한 경우 idx는 0, 일치하는 id가 없는 경우 -1
+
+    if (idx !== -1) {
+      discussionsData.splice(idx, 1); // idx부터 요소 1개 삭제
+      return res.status(202).send(`deleted id: ${id}`)
+    } else {
+      return res.status(404).send("Not Found");
+    }
   }
 
 };
